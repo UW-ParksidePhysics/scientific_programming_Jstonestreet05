cdc9324acf5df32ef396a5a197ca5e8db2be62d6
Jstonestreet05 <stonestreetjake@gmail.com>
2026-02-17 07:44:10 -0600

    Update add_two_numbers.py

--- python/computing_with_formulas/add_two_numbers.py
+++ python/computing_with_formulas/add_two_numbers.py
@@ -1,7 +1,4 @@
-first_number = 5.
-# print(f'Type of first number is: {type(first_number)}')
-second_number = 7
-
-summation = first_number + second_number
-print(f' The sum of {first_number} and {second_number} is {summation}.')
-
+Var_birthmonth = 5
+Var_birthday = 5
+print("birth month + birthday=")
+print(Var_birthmonth+Var_birthday)
